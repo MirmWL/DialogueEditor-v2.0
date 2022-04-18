a0709dabd4df68bf3351a0f1632003b0896863f4
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-04-18 20:12:43 +0300

    mousePosition cashed

--- Assets/DialogueEditor/Editor/Factory/SpeechNodeFactory.cs
+++ Assets/DialogueEditor/Editor/Factory/SpeechNodeFactory.cs
@@ -1,23 +1,22 @@
-﻿using UnityEngine;
-
+﻿
 public class SpeechNodeFactory : IFactory<INode>
 {
     private readonly CustomSimpleTexture2D _texture;
     private readonly IPosition _draggerPosition;
+    private readonly IPosition _mousePosition;
     private int _currentIndex;
     
     public SpeechNodeFactory(CustomSimpleTexture2D texture, IPosition draggerPosition)
     {
         _texture = texture;
         _draggerPosition = draggerPosition;
+        _mousePosition = new MousePosition();
     }
 
     public INode Create(ReferenceRect rect)
     {
-        var mousePosition = new MousePosition();
-        
-        var clickInput = new PredicateDependentInput(new InRect(rect, mousePosition), new Click());
-        var dragInput = new PredicateDependentInput(new InRect(rect, mousePosition), new Drag());
+        var clickInput = new PredicateDependentInput(new InRect(rect, _mousePosition), new Click());
+        var dragInput = new PredicateDependentInput(new InRect(rect, _mousePosition), new Drag());
         
         return new SpeechNode(clickInput, dragInput, _draggerPosition, _texture, rect, _currentIndex++);
     }
