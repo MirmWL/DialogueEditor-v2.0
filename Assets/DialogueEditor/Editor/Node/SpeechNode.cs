6ff4c782025b57980d33b4aa1899fa6f7b5232f5
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-04-18 20:09:39 +0300

    remove OptionNodeFactory.cs

--- Assets/DialogueEditor/Editor/Node/SpeechNode.cs
+++ Assets/DialogueEditor/Editor/Node/SpeechNode.cs
@@ -27,7 +27,7 @@ public class SpeechNode : INode
         var offset = new Vector2(-_rect.Get().width / 2, -_rect.Get().height / 2);
         
         GUI.DrawTexture(_rect.Get(), _texture);
-       //Debug.Log($"{_index} || position: {_rect.Get()} || texture: {_texture}");
+        Debug.Log($"{_index} || position: {_rect.Get()} || texture: {_texture}");
 
         if (_clickInput.HasInput())
             Debug.Log("click");
