f3c03c28bb984cb939d0e4b4822f34e3c2160729
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-04-25 23:20:38 +0300

    add CustomRect.cs

---
+++ Assets/DialogueEditor/Editor/Rect/CustomRect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CustomRect
+{
+    private readonly IPosition _position;
+    private readonly IPosition _size;
+
+    public CustomRect(IPosition position, IPosition size)
+    {
+        _position = position;
+        _size = size;
+    }
+
+    public Rect Get()
+    {
+        return new Rect(_position.Get(), _size.Get());
+    }
+}
