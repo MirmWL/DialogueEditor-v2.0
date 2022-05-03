896c0997ec36066c260bc9bf0d11bbf8f76c4cea
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-03 16:50:15 +0300

    add X/Y Vectors

---
+++ Assets/DialogueEditor/Editor/Position/XVector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class XVector : IPosition
+{
+    private readonly IPosition _position;
+    
+    public XVector(IPosition position)
+    {
+        _position = position;
+    }
+    
+    public Vector2 Get()
+    {
+        return new Vector2(_position.Get().x, 0);
+    }
+}
---
+++ Assets/DialogueEditor/Editor/Position/YVector.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class YVector : IPosition
+{
+    private readonly IPosition _position;
+
+    public YVector(IPosition position)
+    {
+        _position = position;
+    }
+
+    public Vector2 Get()
+    {
+        return new Vector2(0, _position.Get().y);
+    }
+}
