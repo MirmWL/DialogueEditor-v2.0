d8b443a0ce8d65ac0c9c9bb9a57d98e25489896d
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-03 16:48:43 +0300

    add RectFork.cs

---
+++ Assets/DialogueEditor/Editor/Rect/RectFork.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class RectFork : IRect
+{
+    private readonly IPredicate _predicate;
+    private readonly IRect _first;
+    private readonly IRect _second;
+
+    public RectFork(IPredicate predicate, IRect first, IRect second)
+    {
+        _predicate = predicate;
+        _first = first;
+        _second = second;
+    }
+
+    public Rect Get()
+    {
+        return _predicate.Execute() ? _first.Get() : _second.Get();
+    }
+}
