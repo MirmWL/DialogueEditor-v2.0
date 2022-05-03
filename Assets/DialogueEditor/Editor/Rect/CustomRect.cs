3520e8e17c6706080116cfa3597552b96a0bb651
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-03 16:49:03 +0300

    update CustomRect.cs

--- Assets/DialogueEditor/Editor/Rect/CustomRect.cs
+++ Assets/DialogueEditor/Editor/Rect/CustomRect.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 
-public class CustomRect : IRect
+public class CustomRect : IRect, IUpdate
 {
     private readonly IPosition _position;
     private readonly IPosition _size;
 
+    private Rect _rect;
+    
     public CustomRect(IPosition position, IPosition size)
     {
         _position = position;
         _size = size;
+        _rect = new Rect(position.Get(), size.Get());
     }
 
     public Rect Get()
     {
-        return new Rect(_position.Get(), _size.Get());
+        return _rect;
+    }
+
+    public void Update()
+    {
+        _rect = new Rect(_position.Get(), _size.Get());
     }
 }
