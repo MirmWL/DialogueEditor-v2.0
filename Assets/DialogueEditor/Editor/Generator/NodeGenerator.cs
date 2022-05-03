f3f4def3a83463b2e363c3774a018582d354bc06
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-03 16:50:41 +0300

    update NodeGenerator.cs

--- Assets/DialogueEditor/Editor/Generator/NodeGenerator.cs
+++ Assets/DialogueEditor/Editor/Generator/NodeGenerator.cs
@@ -4,59 +4,66 @@ using UnityEngine;
 
 public class NodeGenerator : IUpdate
 {
-    private readonly ReferenceRect _panelRect;
-    private readonly ReferenceRect _createButtonRect;
+    private readonly IRect _panelRect;
+    private readonly IRect _createButtonRect;
+    private readonly IPosition _nodeDraggerPosition;
+    private readonly IRect _editNodePanelRect;
+    private readonly INodeFactory _nodeFactory;
+
     private readonly Texture2D _texture;
     private readonly Updates _updates;
-    
-    private readonly Storage<INode> _nodeFactory;
-    
-    public NodeGenerator(Storage<INode> nodeFactory, Updates updates, ITexture2D texture, 
-        ReferenceRect panelRect, ReferenceRect createButtonRect)
+
+    public NodeGenerator(INodeFactory nodeFactory, Updates updates, ITexture2D texture, 
+        IRect panelRect, IRect createButtonRect, IPosition nodeDraggerPosition, IRect editNodePanelRect)
     {
         _nodeFactory = nodeFactory;
         _updates = updates;
         _texture = texture.Get();
         _panelRect = panelRect;
         _createButtonRect = createButtonRect;
+        _nodeDraggerPosition = nodeDraggerPosition;
+        _editNodePanelRect = editNodePanelRect;
     }
-
-    public IEnumerable<INode> Nodes => _nodeFactory.Objects;
     
     public void Update()
     {
-        var panelRectPosition = _panelRect.Get();
         var createButtonRectPosition = _createButtonRect.Get();
-        
-        _panelRect.Get() = new Rect(
-            new Vector2(Screen.width - panelRectPosition.width, panelRectPosition.y),
-            new Vector2(panelRectPosition.width, Screen.height));
-        
-        _createButtonRect.Get() = new Rect(
-            new Vector2(Screen.width - createButtonRectPosition.width, createButtonRectPosition.y), 
-            new Vector2(createButtonRectPosition.width, createButtonRectPosition.height));
-        
-        GUI.DrawTexture(panelRectPosition, _texture);
+
+        GUI.DrawTexture(_panelRect.Get(), _texture);
         
         EditorGUILayout.BeginVertical();
 
         if (GUI.Button(createButtonRectPosition, "Create node"))
-        {
-            var rectPosition = new ReferencePosition(new Vector2(300, 300));
-            var rectSize = new Vector2(100, 100);
-            
-            var rect = new ReferenceRect(new Rect(rectPosition.Get(), rectSize));
+        { 
+            var nodeRectSize = new Vector2(300, 300);
+            var nodeOffset = new PositionAdapter(new Vector2(-nodeRectSize.x / 2, -nodeRectSize.y / 2));
+            var nodeRectPosition = new OffsetPosition(_nodeDraggerPosition, nodeOffset);
 
-            var dragOffset = new PositionWrap(new Vector2(50, 0));
-            var dragOffsetPosition = new OffsetPosition(new PositionAdapter(rectPosition), dragOffset);
-            var dragSize = new PositionWrap(new Vector2(50, 50));
+            var nodeUnpinnedRect = new CustomRect(nodeRectPosition, new PositionAdapter(nodeRectSize));
             
-            var dragRect = new ReferenceRect(new CustomRect(dragOffsetPosition, dragSize).Get());
+            var pinPredicate = new InputDependentPredicate(
+                new MouseUpInput(), 
+                new InRect(_editNodePanelRect, nodeRectPosition));
+
+            var nodeRect = new RectFork(pinPredicate, _editNodePanelRect, nodeUnpinnedRect);
+
+            var dragOffset = new PositionAdapter(new Vector2(50, 0));
+            var dragOffsetPosition = new OffsetPosition(nodeRectPosition, dragOffset);
+            var dragSize = new PositionAdapter(new Vector2(50, 50));
             
-            var node = _nodeFactory.Create(rect, dragRect);
-            _updates.Add(node);
+            var dragUnpinnedRect = new CustomRect(dragOffsetPosition, dragSize);
+            
+            var node = _nodeFactory.Create(nodeRect, dragUnpinnedRect);
+
+            var mouseDownInput = new EventInput(new MouseDownInput());
+            
+            var updateNodeRect = new InputDependentUpdate(mouseDownInput, nodeUnpinnedRect);
+            var updateDragRect = new InputDependentUpdate(mouseDownInput, dragUnpinnedRect);
+            
+            _updates.Add(updateDragRect, updateNodeRect, node);
         }
         
         EditorGUILayout.EndVertical();
     }
+    
 }
