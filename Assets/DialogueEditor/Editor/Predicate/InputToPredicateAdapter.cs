bf2d1e2c150fbb7c2d3afcf00dab17ab6ddc70e4
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-04 21:19:43 +0300

    add predicateAdapter

---
+++ Assets/DialogueEditor/Editor/Predicate/InputToPredicateAdapter.cs
@@ -0,0 +1,14 @@
+﻿public class InputToPredicateAdapter : IPredicate
+{
+    private readonly IInput _input;
+
+    public InputToPredicateAdapter(IInput input)
+    {
+        _input = input;
+    }
+
+    public bool Execute()
+    {
+        return _input.HasInput();
+    }
+}
