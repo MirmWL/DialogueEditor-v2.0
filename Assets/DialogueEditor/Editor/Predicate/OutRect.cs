17cd6ffc3652cafa16676ebd9761430ccf6de83b
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-09 21:26:48 +0300

    add OutRect.cs

---
+++ Assets/DialogueEditor/Editor/Predicate/OutRect.cs
@@ -0,0 +1,16 @@
+﻿public class OutRect : IPredicate
+{
+    private readonly IRect _rect;
+    private readonly IPosition _position;
+
+    public OutRect(IRect rect, IPosition position)
+    {
+        _rect = rect;
+        _position = position;
+    }
+
+    public bool Execute()
+    {
+        return _rect.Get().Contains(_position.Get()) == false;
+    }
+}
