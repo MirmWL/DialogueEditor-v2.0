40ea5b5ff2feee59f6bc0d5a9ff0b488895bd6f8
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-14 22:56:12 +0300

    add Predicates.cs

---
+++ Assets/DialogueEditor/Editor/Predicate/Predicates.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Predicates : IPredicate
+{
+    private readonly IEnumerable<IPredicate> _predicates;
+
+    public Predicates(IEnumerable<IPredicate> predicates)
+    {
+        _predicates = predicates;
+    }
+
+    public bool Execute()
+    {
+        return _predicates.All(predicate => predicate.Execute());
+    }
+}
