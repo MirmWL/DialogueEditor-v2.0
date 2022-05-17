c9fb51a69381c45b7ddc40d97ab73e39e12337df
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-17 16:40:56 +0300

    add CachedPredicate.cs

---
+++ Assets/DialogueEditor/Editor/Predicate/CachedPredicate.cs
@@ -0,0 +1,21 @@
+﻿public class CachedPredicate : IPredicate
+{
+    private readonly IPredicate _returnCached;
+    private readonly IPredicate _predicate;
+    
+    private bool _cached;
+
+    public CachedPredicate(IPredicate returnCached, IPredicate predicate)
+    {
+        _returnCached = returnCached;
+        _predicate = predicate;
+    }
+
+    public bool Execute()
+    {
+        var result = _returnCached.Execute() ? _cached : _predicate.Execute();
+
+        _cached = result;
+        return result;
+    }
+}
