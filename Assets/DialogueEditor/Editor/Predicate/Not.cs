0b8e6f74dd8dfcf6ef652de7e74d93673f352afe
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-17 16:41:13 +0300

    add Not.cs

---
+++ Assets/DialogueEditor/Editor/Predicate/Not.cs
@@ -0,0 +1,14 @@
+﻿public class Not : IPredicate
+{
+    private readonly IPredicate _predicate;
+
+    public Not(IPredicate predicate)
+    {
+        _predicate = predicate;
+    }
+
+    public bool Execute()
+    {
+        return !_predicate.Execute();
+    }
+}
