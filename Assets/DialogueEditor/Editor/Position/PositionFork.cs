99ba8b1a9ca26ae66718b23279c167a039a10105
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-23 17:40:01 +0300

    add PositionFork.cs

---
+++ Assets/DialogueEditor/Editor/Position/PositionFork.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PositionFork : IPosition
+{
+    private readonly IPredicate _predicate;
+    private readonly IPosition _first;
+    private readonly IPosition _second;
+
+    public PositionFork(IPredicate predicate, IPosition first, IPosition second)
+    {
+        _predicate = predicate;
+        _first = first;
+        _second = second;
+    }
+
+    public Vector2 Get()
+    {
+        return _predicate.Execute() ? _first.Get() : _second.Get();
+    }
+}
