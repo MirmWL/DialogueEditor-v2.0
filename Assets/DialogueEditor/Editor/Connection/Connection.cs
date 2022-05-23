f554bf55e3435ad84eea2939bbe82744fc1699f0
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-23 17:13:59 +0300

    add Connection.cs

---
+++ Assets/DialogueEditor/Editor/Connection/Connection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public class Connection : IUpdate
+{
+    private readonly KeyValuePair<INode, INode> _pair;
+    public void Update()
+    {
+        
+    }
+}
