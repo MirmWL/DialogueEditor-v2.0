8acf7344d210b6e416ecffcbaf8502cd6aa20345
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-23 17:23:58 +0300

    update Connection.cs

--- Assets/DialogueEditor/Editor/Connection/Connection.cs
+++ Assets/DialogueEditor/Editor/Connection/Connection.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 public class Connection : IUpdate
 {
-    private readonly KeyValuePair<INode, INode> _pair;
+    private readonly KeyValuePair<IRect, IRect> _connection;
+    
+    public Connection(KeyValuePair<IRect, IRect> connection)
+    {
+        _connection = connection;
+    }
+
     public void Update()
     {
+        var firstPoint = _connection.Key.Get().position;
+        var secondPoint = _connection.Value.Get().position;
         
+        Handles.DrawLine(firstPoint, secondPoint);
     }
 }
