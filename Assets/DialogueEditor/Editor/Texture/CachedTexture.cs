e4a12de04594290feae7a54635bcdfb15fc15d72
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-30 16:08:49 +0300

    add CachedTexture.cs

---
+++ Assets/DialogueEditor/Editor/Texture/CachedTexture.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CachedTexture : ITexture2D
+{
+    private readonly IPredicate _returnCached;
+    private readonly ITexture2D _texture;
+    
+    private ITexture2D _cached;
+
+    public CachedTexture(IPredicate returnCached, ITexture2D texture)
+    {
+        _returnCached = returnCached;
+        _texture = texture;
+        _cached = _texture;
+    }
+
+    public Texture2D Get()
+    {
+        var result = _returnCached.Execute() ? _cached : _texture;
+
+        _cached = result;
+        return result.Get();
+    }
+}
