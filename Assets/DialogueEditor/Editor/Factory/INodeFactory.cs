ed4da863ae0f0846c1354df7d80e0f2b93037c40
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-05-30 17:28:02 +0300

    replace nodeInitialization to nodeFactory

--- Assets/DialogueEditor/Editor/Factory/INodeFactory.cs
+++ Assets/DialogueEditor/Editor/Factory/INodeFactory.cs
@@ -2,5 +2,5 @@
 
 public interface INodeFactory
 {
-    INode Create(IRect rect, IRect dragRect,IRect createConnectionButtonRect, IPredicate pinnedPredicate, IInput clickInput);
+    INode Create();
 }
