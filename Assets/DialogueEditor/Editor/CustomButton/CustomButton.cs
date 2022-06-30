250358c5b6b74f1dafd56311df3bc8e45ef0d010
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-06-30 18:16:09 +0300

    add CustomButton.cs

---
+++ Assets/DialogueEditor/Editor/CustomButton/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CustomButton : IUpdate
+{
+    private readonly IRect _rect;
+    private readonly ITexture2D _texture;
+    private readonly string _text;
+
+    public CustomButton(IRect rect, ITexture2D texture, string text)
+    {
+        _rect = rect;
+        _texture = texture;
+        _text = text;
+    }
+
+    public void Update()
+    {
+        var rect = _rect.Get();
+        
+        GUI.DrawTexture(rect, _texture.Get());
+        GUI.Label(rect, _text);
+    }
+}
---
+++ Assets/DialogueEditor/Editor/CustomButton/CustomButtonFactory.cs
@@ -0,0 +1,18 @@
+﻿public class CustomButtonFactory
+{
+    private readonly ITexture2D _texture;
+    private readonly string _text;
+
+    public CustomButtonFactory(ITexture2D texture, string text)
+    {
+        _texture = texture;
+        _text = text;
+    }
+
+    public CustomButton Create(IRect rect)
+    {
+        var button = new CustomButton(rect, _texture, _text);
+        return button;
+    }
+    
+}
