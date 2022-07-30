4d9501ffa93d0c5d40b9cc01ca23d9ad55a72304
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-07-30 22:32:46 +0300

    add CachedRect.cs

---
+++ Assets/DialogueEditor/Editor/Rect/CachedRect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CachedRect : IRect
+{
+    private readonly IRect _rect;
+    private readonly IPredicate _returnCached;
+    private IRect _cached;
+    
+    public CachedRect(IPredicate returnCached, IRect rect)
+    {
+        _rect = rect;
+        _returnCached = returnCached;
+        _cached = rect;
+    }
+
+    public Rect Get()
+    {
+        var result = _returnCached.Execute() ? _cached : _rect;
+
+        _cached = result;
+        return result.Get();
+    }
+}
