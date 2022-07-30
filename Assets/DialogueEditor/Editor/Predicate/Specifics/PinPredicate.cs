25175b20d16ad19fbcf7da39d1b1d4b738445d89
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-07-30 23:06:03 +0300

    replace PinPredicateFactory.cs with PinPredicate.cs

--- Assets/DialogueEditor/Editor/Predicate/Specifics/PinPredicate.cs
+++ Assets/DialogueEditor/Editor/Predicate/Specifics/PinPredicate.cs
@@ -1,34 +1,37 @@
 ﻿using EditorInput;
 
-public class PinPredicateFactory
+public class PinPredicate : IPredicate
 {
     private readonly IPosition _nodeDraggerPosition;
     private readonly IPosition _dragUnpinnedPosition;
     private readonly IRect _editNodePanelRect;
     private readonly IInput _dragInput;
+    private readonly IRect _dragUnpinnedRect;
 
-    public PinPredicateFactory(
+    public PinPredicate(
         IPosition nodeDraggerPosition,
         IPosition dragUnpinnedPosition,
         IRect editNodePanelRect,
-        IInput dragInput)
+        IInput dragInput, 
+        IRect dragUnpinnedRect)
     {
         _nodeDraggerPosition = nodeDraggerPosition;
         _dragUnpinnedPosition = dragUnpinnedPosition;
         _editNodePanelRect = editNodePanelRect;
         _dragInput = dragInput;
+        _dragUnpinnedRect = dragUnpinnedRect;
     }
 
-    public IPredicate Create(IRect dragUnpinnedRect)
+    public bool Execute()
     {
         var inEditPanel = new Predicates(
-            new InRect(dragUnpinnedRect, _nodeDraggerPosition), 
+            new InRect(_dragUnpinnedRect, _nodeDraggerPosition), 
             new InRect(_editNodePanelRect, _dragUnpinnedPosition));
         
         var pin = new CachedPredicate(
             new Not(new InputToPredicateAdapter(_dragInput)),
             inEditPanel);
         
-        return pin;
+        return pin.Execute();
     }
 }
