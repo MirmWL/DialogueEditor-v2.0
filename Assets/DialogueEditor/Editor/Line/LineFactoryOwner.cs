0cdd84a7e541d0ae5c28e9d0ccaa922cd0804070
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-08-14 22:37:28 +0300

    rename to "owner"

---
+++ Assets/DialogueEditor/Editor/Line/LineFactoryOwner.cs
@@ -0,0 +1,19 @@
+﻿using EditorInput;
+
+public class LineFactoryOwner : IUpdate
+{
+    private readonly ICondition _createCondition;
+    private readonly LineFactory _factory;
+    
+    public LineFactoryOwner(ICondition createCondition, LineFactory factory)
+    {
+        _createCondition = createCondition;
+        _factory = factory;
+    }
+    
+    public void Update()
+    {
+        if (_createCondition.Execute())
+            _factory.Create();
+    }
+}
