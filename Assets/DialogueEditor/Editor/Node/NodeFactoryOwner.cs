512e71eb407e0ed8bff6af6f8df23e1e2992c24a
Miroslav Momotov <momotovmiroslav@gmail.com>
2022-08-14 22:31:05 +0300

    add NodeFactoryOwner

---
+++ Assets/DialogueEditor/Editor/Node/NodeFactoryOwner.cs
@@ -0,0 +1,22 @@
+﻿public class NodeFactoryOwner : IUpdate
+{
+    private readonly ICondition _create;
+    private readonly NodeFactory _nodeFactory;
+    private readonly Updates _updates;
+
+    public NodeFactoryOwner(
+        ICondition create,
+        NodeFactory nodeFactory,
+        Updates updates)
+    {
+        _create = create;
+        _nodeFactory = nodeFactory;
+        _updates = updates;
+    }
+
+    public void Update()
+    {
+        if (_create.Execute())
+            _updates.Add(_nodeFactory.Create());
+    }
+}
